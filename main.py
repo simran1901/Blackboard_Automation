57f4ca0cdbe247e094d89730f1a02e0b246d63cd
simran1901 <simranmakhijani55@gmail.com>
2020-10-30 18:44:38 +0530

    set chrome options

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ global sessions
 sessions = 0
 # To get the text from messages
 span = ""
+
+# Set Chrome options
+opt = Options()
+opt.add_argument("--disable-infobars")
+opt.add_argument("start-maximized")
+opt.add_argument('--no-sandbox')
+opt.add_argument('--ignore-certificate-errors-spki-list')
+opt.add_argument('--ignore-ssl-errors')
+# opt.add_argument("--incognito")
+opt.add_experimental_option("prefs", {
+                                        "profile.default_content_setting_values.media_stream_mic": 1,
+                                        "profile.default_content_setting_values.media_stream_camera": 1,
+                                        "profile.default_content_setting_values.geolocation": 2,
+                                        "profile.default_content_setting_values.notifications": 2,
+})
+opt.add_argument("--disable-extensions")
+
