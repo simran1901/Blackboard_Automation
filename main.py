a3757559704082af1b24e3a365c11cdde87363a4
simran1901 <simranmakhijani55@gmail.com>
2020-10-30 18:43:06 +0530

    added sessions variable

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 import time
 from datetime import datetime
+
+# To count the number of sessions
+global sessions
+sessions = 0
+# To get the text from messages
+span = ""
